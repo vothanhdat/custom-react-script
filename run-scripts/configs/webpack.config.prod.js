ccf5ca3e6f4b47666478928d476b282a7b166f95
Vo Thanh Dat <vothanhdat@outlook.com>
2018-07-08 00:01:24 +0700

    update

--- run-scripts/configs/webpack.config.prod.js
+++ run-scripts/configs/webpack.config.prod.js
@@ -20,7 +20,7 @@ const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
 
 const {
-    prerenderPaths = ['/'],
+  prerenderPaths = ['/'],
 } = require('./projectconfig');
 
 
@@ -28,103 +28,99 @@ const {
 
 
 module.exports = () => require('./webpack.config')({
-    dev: false,
-    babel_plugins: [
-        "@babel/plugin-proposal-object-rest-spread",
-        "@babel/plugin-transform-runtime",
-        "@babel/plugin-transform-arrow-functions",
-        ["transform-imports", {
-            "material-ui/styles": {
-                "transform": "material-ui/styles/${member}",
-                "preventFullImport": true
-            },
-            "material-ui": {
-                "transform": "material-ui/${member}",
-                "preventFullImport": true
-            },
-            "lodash": {
-                "transform": "lodash/${member}",
-                "preventFullImport": true
-            },
-            "lodash-decorators": {
-                "transform": "lodash-decorators/${member}",
-                "preventFullImport": true
-            },
-        }]
-    ],
-    plugins: [
-        new UglifyJsPlugin({
-            sourceMap: false,
-        }),
-        new CleanWebpackPlugin([resolveApp('./build')], { root: process.cwd() }),
-        new CopyWebpackPlugin([
-            { from: 'static/', to: '' },
-            { from: 'static/index.html', to: '200.html' },
-        ]),
-        new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
-        new MiniCssExtractPlugin({
-            filename: "style.[name].[hash].css",
-        }),
-        new PrerenderSPAPlugin({
-            staticDir: resolveApp('./build'),
-            routes: prerenderPaths,
-            renderer: new Renderer({
-                injectProperty: '__PRERENDER_SPA',
-                inject: {},
-                // headless: false // Display the browser window when rendering. Useful for debugging.
-            }),
-            minify: {
-                collapseBooleanAttributes: true,
-                collapseWhitespace: true,
-                decodeEntities: true,
-                keepClosingSlash: true,
-                sortAttributes: true,
-                removeComments: true,
-                minifyJS: true,
-                minifyCSS: true,
-            },
-        }),
-        new HtmlWebpackPlugin({
-            filename: '200.html',
-            template: 'static/index.html',
-            chunks: ['vendors~main', 'main']
-        }),
-        new ProgressBarPlugin(),
-        new BundleAnalyzerPlugin(),
+  dev: false,
+  babel_plugins: [
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-transform-arrow-functions",
+    ["transform-imports", {
+      "@material-ui/core": {
+        "transform": "material-ui/core/${member}",
+        "preventFullImport": true
+      },
+      "lodash": {
+        "transform": "lodash/${member}",
+        "preventFullImport": true
+      },
+      "lodash-decorators": {
+        "transform": "lodash-decorators/${member}",
+        "preventFullImport": true
+      },
+    }]
+  ],
+  plugins: [
+    new UglifyJsPlugin({
+      sourceMap: false,
+    }),
+    new CleanWebpackPlugin([resolveApp('./build')], { root: process.cwd() }),
+    new CopyWebpackPlugin([
+      { from: 'static/', to: '' },
+      { from: 'static/index.html', to: '200.html' },
+    ]),
+    new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
+    new MiniCssExtractPlugin({
+      filename: "style.[name].[hash].css",
+    }),
+    new PrerenderSPAPlugin({
+      staticDir: resolveApp('./build'),
+      routes: prerenderPaths,
+      renderer: new Renderer({
+        injectProperty: '__PRERENDER_SPA',
+        inject: {},
+        // headless: false // Display the browser window when rendering. Useful for debugging.
+      }),
+      minify: {
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        decodeEntities: true,
+        keepClosingSlash: true,
+        sortAttributes: true,
+        removeComments: true,
+        minifyJS: true,
+        minifyCSS: true,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      filename: '200.html',
+      template: 'static/index.html',
+      chunks: ['vendors~main', 'main']
+    }),
+    new ProgressBarPlugin(),
+    new BundleAnalyzerPlugin(),
 
-    ],
-    cssExtra: ([cssloader, ...rest]) => [
-        MiniCssExtractPlugin.loader,
-        {
-            ...cssloader,
-            options: {
-                ...cssloader.options,
-                importLoaders: 2,
-            }
-        },
-        {
-            loader: 'postcss-loader',
-            options: {
-                config: {
-                    path: __dirname + '/postcss.config.js'
-                }
-            }
-        },
-        ...rest,
-    ],
-    babelPresets: [
-        ["@babel/preset-env", {
-            targets: {
-                browsers: ["last 2 versions"]
-            },
-            shippedProposals: true
-        }],
-    ],
-    override: {
-        stats: {
-            children: false,
-            colors: true,
-        },
-    }
+  ],
+  cssExtra: ([cssloader, ...rest]) => [
+    MiniCssExtractPlugin.loader,
+    {
+      ...cssloader,
+      options: {
+        ...cssloader.options,
+        importLoaders: 2,
+      }
+    },
+    {
+      loader: 'postcss-loader',
+      options: {
+        config: {
+          path: __dirname + '/postcss.config.js'
+        }
+      }
+    },
+    ...rest,
+  ],
+  babelPresets: [
+    ["@babel/preset-env", {
+      targets: {
+        browsers: ["last 2 versions"]
+      },
+      shippedProposals: true
+    }],
+  ],
+  override: {
+    stats: {
+      children: false,
+      colors: true,
+    },
+  }
 })
 
